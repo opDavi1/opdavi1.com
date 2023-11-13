869e288fef9e64d449a97e65611867e44b9f55e3
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2023-11-12 19:02:54 -0800

    basic server functionality for index.html

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.use("/assets", express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"), (err) => {
+        if(err) {
+            res.sendStatus(500);
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`);
 });
