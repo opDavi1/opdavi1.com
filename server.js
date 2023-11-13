0756073919c318c7b20d8fede05eed6ba6c8c8b5
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2023-11-13 00:31:36 -0800

    added image gallery server code and link to gallery on index.html

--- server.js
+++ server.js
@@ -4,11 +4,29 @@ const app = express();
 const PORT = 3000;
 
 app.use("/assets", express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"), (err) => {
         if(err) {
             res.sendStatus(500);
+        }
+    });
+});
+
+app.get("/image-gallery", (req, res) => {
+     res.sendFile(path.join(__dirname, "views", `img-gallery1.html`), (err) => {
+        if(err) {
+            res.sendStatus(500);
+        }
+    });
+})
+
+app.get("/image-gallery/:page", (req, res) => {
+    let page = req.params.page;
+    res.sendFile(path.join(__dirname, "views", `img-gallery${page}.html`), (err) => {
+        if(err) {
+            res.sendStatus(404);
         }
     });
 });
